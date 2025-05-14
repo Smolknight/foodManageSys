cf01c09f1a4d816e51e1dc2c064d0085af5a786d
Matthew Mendoza <kylethechickenman02@gmail.com>
2025-05-13 17:12:38 -0700

    time now saved to session storage

--- timer.js
+++ timer.js
@@ -1,6 +1,5 @@
 let timeDisplay=document.getElementById('timer')
 let time={
-    days:0,
     hours:0,
     minutes:0,
     seconds:0
@@ -8,20 +7,32 @@ let time={
 let seconds=time.seconds
 let minutes=time.minutes
 let hours=time.hours
-
 setInterval(function(){
     seconds++
-    if(seconds==60){
+    time.seconds=seconds
+    if(minutes==60){
+        hours++
+        time.hours=hours
+        minutes=0
+    }else if(seconds==60){
         minutes++
+        time.minutes=minutes
         seconds=0
+        time.seconds=seconds
     }
     displayTime()
+    let timeData=JSON.stringify(time)
+    sessionStorage.setItem("sessionTime",timeData)
 },1000)
 
 function displayTime(){
-    if(minutes>0){
-        timeDisplay.innerText=`time spent on system: ${minutes} minute(s) ${seconds} seconds(s)`
+    if(hours>0){
+        timeDisplay.innerText=`time spent on system: ${hours} hour(s) ${minutes} minute(s) ${seconds} second(s)`
+    }
+    else if(minutes>0){
+        timeDisplay.innerText=`time spent on system: ${minutes} minute(s) ${seconds} second(s)`
     }else{
-        timeDisplay.innerText=`time spent on system: ${seconds} seconds(s)`
+        timeDisplay.innerText=`time spent on system: ${seconds} second(s)`
     }
 }
+
