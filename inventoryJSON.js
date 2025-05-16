b71d03badb21008cc8ae858219848955a03a1971
Matthew Mendoza <kylethechickenman02@gmail.com>
2025-05-15 17:18:07 -0700

    forgot to include json

---
+++ inventoryJSON.js
@@ -0,0 +1,43 @@
+const items=[
+    {
+        "id":1,
+        "name":"apples",
+        "SKU":"FR-APP-RED",
+        "quantity":500,
+        "group":"fruit",
+        "parLvl":230
+    },
+    {
+        "id":2,
+        "name":'pears',
+        "SKU":"FR-PEAR-GR",
+        "quantity":300,
+        "group":'fruit',
+        "parLvl":100    
+    },
+    {
+        "id":3,
+        "name":"gallon of full milk",
+        "SKU":"MLK-FULL-1G",
+        "quantity":60,
+        "group":"dairy",
+        "parLvl":20
+    },
+    {
+        "id":4,
+        "name":'carton of soy milk',
+        "SKU":'MLK-SOY-CART',
+        "quantity":20,
+        "group":'dairy',
+        'parLvl':10
+    },
+    {
+        "id":5,
+        "name":"eggs",
+        "SKU":"EGG-12-GA",
+        "quantity":100,
+        "group":"poultry",
+        "parLvl":20
+    }
+]
+const itemData=JSON.stringify(items)
